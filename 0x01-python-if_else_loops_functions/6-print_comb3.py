c81bddf6ef511a7be66241ff83cc2fa000f7f25b
pheyishayo <amzatsayi@yahoo.com>
2023-11-28 12:08:29 -0800

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for first_number in range(0, 10):
+    for last_number in range(first_number + 1, 10):
+        if first_number == 8 and last_number == 9:
+            print("{}{}".format(first_number, last_number))
+        else:
+            print("{}{}".format(first_number, last_number), end=", ")
