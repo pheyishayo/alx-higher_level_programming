a1652fea6047350e28385140c098dad81236369a
pheyishayo <amzatsayi@yahoo.com>
2023-11-28 13:36:43 -0800

    remove

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return (str)
+
+    result = ""
+    for x in range(len(str)):
+        if x != n:
+            result += str[x]
+
+        return (result)
