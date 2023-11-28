442bbbed3928784dfbb280d96fe018047585205c
pheyishayo <amzatsayi@yahoo.com>
2023-11-28 10:22:09 -0800

    lastdigitstress

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-print(f"Last digit of {number} is {last_digit if number >= 0 else -last_digit}", end=" ")
+
+if number >= 0:
+    sign = ""
+else:
+    sign = "-"
+print(f"Last digit of {number} is {sign}{last_digit}", end=" ")
 
 if last_digit > 5:
     print("and is greater than 5")
